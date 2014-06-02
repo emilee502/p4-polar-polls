43e4a8936a8e8b2cbf9b8e3d876e10957b7fcc91
emilee502 <Oscar@Jesss-MacBook-Air.local>
2014-06-01 17:13:36 -0700

    got files

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$('.con').click(function(){
+
+	$('.belt').animate({
+		'top' : '-740px'
+	},1500, "swing")
+
+});
+
+$('.pri').click(function(){
+
+	$('.belt').animate({
+		'top' : '-1550px'
+	},1500, "swing")
+
+});
+
+$('.men').click(function(){
+
+	$('.belt').animate({
+		'top' : '-2700px'
+	},2000, "swing").animate({
+		'top' : '-2330px'
+	},800, "swing")
+
+});
